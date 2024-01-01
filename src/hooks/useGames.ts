b851f2ccda32a69917beb414bc786be2f442a581
jakefleming13 <jakefleming@shaw.ca>
2023-12-31 17:13:07 -0700

    Started loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -28,27 +28,36 @@ const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
+  const [isLoading, setLoading] = useState(false); //State variable for loading skeletons
+
   //Use 'effect hook' to send a fetch request to the backend
   useEffect(() => {
 
     const controller = new AbortController(); //To handle Cancelations
+
+    setLoading(true); //Set to true right before we call our api
 
     //Call '.get' with the '/games' endpoint & signal object while providing a generic type argument of our interface
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
 
       //'.then' needs interfaces to allow us to call 'setGames' with the proper structure
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if( err instanceof CanceledError) return;
-        setError(err.message)});
+        setError(err.message);
+        setLoading(false);
+      });
 
     return () => controller.abort();
     
       //include an array of dependencies, without this we constantly send requests to our backend
   }, []);
 
-  return {games, error};
+  return {games, error, isLoading};
 }
 
 export default useGames;
