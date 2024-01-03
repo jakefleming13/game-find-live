2bf4dbc3b8bebaff26e4fb2cc4958f58d8a70576
jakefleming13 <jakefleming@shaw.ca>
2024-01-03 10:01:51 -0700

    Implement Searching

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,29 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props{
+  onSearch: (searchText:string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup>
-    <InputLeftElement children={<BsSearch />}/>
-      <Input borderRadius={20} placeholder="Search games..." variant="filled" />
-    </InputGroup>
+
+    //Need a form to handle the search function
+    <form onSubmit={(event) => {
+      event.preventDefault(); //prevents the event from getting access to the server
+
+      //When submitting the form, check:
+      if(ref.current) onSearch(ref.current.value);
+       
+    }}>
+      <InputGroup>
+      <InputLeftElement children={<BsSearch />}/>
+        <Input ref={ref} borderRadius={20} placeholder="Search games..." variant="filled" />
+      </InputGroup>
+    </form>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -20,7 +19,12 @@ export interface Game {
 const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
-    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder } },
+    { params: { 
+      genres: gameQuery.genre?.id, 
+      platforms: gameQuery.platform?.id, 
+      ordering: gameQuery.sortOrder,
+      search: gameQuery.searchText
+     }},
     [gameQuery.genre?.id, gameQuery.platform?.id] //array of dependencies
   );
 
