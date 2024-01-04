de1c81baa13deeb27ea2c368ea2745b035479980
jakefleming13 <jakefleming@shaw.ca>
2024-01-04 10:28:57 -0700

    formatted code

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,26 +2,32 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-interface Props{
-  onSearch: (searchText:string) => void;
+interface Props {
+  onSearch: (searchText: string) => void;
 }
 
 const SearchInput = ({ onSearch }: Props) => {
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-
     //Need a form to handle the search function
-    <form onSubmit={(event) => {
-      event.preventDefault(); //prevents the event from getting access to the server
+    <form
+      onSubmit={(event) => {
+        
+        event.preventDefault(); //prevents the event from getting access to the server
 
-      //When submitting the form, check:
-      if(ref.current) onSearch(ref.current.value);
-       
-    }}>
+        //When submitting the form, check:
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
       <InputGroup>
-      <InputLeftElement children={<BsSearch />}/>
-        <Input ref={ref} borderRadius={20} placeholder="Search games..." variant="filled" />
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
       </InputGroup>
     </form>
   );
