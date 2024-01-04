45e3d807e955dce48c149a9927765f2868b0640f
jakefleming13 <jakefleming@shaw.ca>
2024-01-04 09:58:31 -0700

    updated color cheme

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,30 +1,32 @@
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
-import PlaformIconList from './PlaformIconList';
-import CriticScore from './CriticScore';
-import getCroppedImageurl from '../services/image-url';
-import Emoji from './Emoji';
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import PlaformIconList from "./PlaformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageurl from "../services/image-url";
+import Emoji from "./Emoji";
 
-interface Props{
-    game: Game;
+interface Props {
+  game: Game;
 }
 
-const GameCard = ({game}: Props) => {
+const GameCard = ({ game }: Props) => {
   return (
     <Card>
-        <Image src={getCroppedImageurl(game.background_image)}/>
-        <CardBody>
-            
-
-            <HStack justifyContent='space-between' marginBottom={3}>
-                {/* This mapping is constructing an array of platform objects */}
-                <PlaformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
-                <CriticScore score={game.metacritic}/>
-            </HStack>
-            <Heading fontSize='2xl'>{game.name} <Emoji rating={game.rating_top}/> </Heading>
-        </CardBody>
+      <Image src={getCroppedImageurl(game.background_image)} />
+      <CardBody>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          {/* This mapping is constructing an array of platform objects */}
+          <PlaformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading fontSize="2xl">
+          {game.name} <Emoji rating={game.rating_top} />{" "}
+        </Heading>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
