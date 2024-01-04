0c53ebcfdb768471dee5c4e6d67dd28ae3d1b1bf
jakefleming13 <jakefleming@shaw.ca>
2024-01-03 18:06:00 -0700

    cleaned up game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,14 @@ const GameCard = ({game}: Props) => {
     <Card>
         <Image src={getCroppedImageurl(game.background_image)}/>
         <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
+            
 
-            <HStack justifyContent='space-between'>
+            <HStack justifyContent='space-between' marginBottom={3}>
                 {/* This mapping is constructing an array of platform objects */}
                 <PlaformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
                 <CriticScore score={game.metacritic}/>
             </HStack>
+            <Heading fontSize='2xl'>{game.name}</Heading>
         </CardBody>
     </Card>
   )
