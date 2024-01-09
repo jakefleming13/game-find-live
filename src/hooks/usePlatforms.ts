21c7c71f834037f916decf5b32ec4262401573ad
jakefleming13 <jakefleming@shaw.ca>
2024-01-08 20:46:54 -0700

    refactored fetching of platforms and genres

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 interface Platform{
     id:number;
@@ -7,6 +10,15 @@ interface Platform{
 }
 
 //Call our useData hook with a new endpoint to fetch the info we need
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => 
+        apiClient
+            .get<FetchResponse<Platform>>('/platforms/lists/parents')
+            .then(res => res.data),
+
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: {count: platforms.length, results: platforms}
+})
 
 export default usePlatforms;
