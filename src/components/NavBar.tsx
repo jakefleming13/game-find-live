8310cb3377a2f7bf2c48349566c8c7105f92d33c
jakefleming13 <jakefleming@shaw.ca>
2024-01-12 19:40:59 -0700

    added new logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,13 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import newLogo from "../assets/newLogo.webp";
 
 const NavBar = () => {
   return (
     //HStack: for laying out components horizontally
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src= {newLogo} boxSize="60px" />
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
